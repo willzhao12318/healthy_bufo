3ae08419d4a553ea2f9721b4211b0a4f7eb876e5
Will Zhao <wzhao2@flexport.com>
2024-12-02 21:24:22 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,6 +27,9 @@ const Home = () => {
     <>
       <Head>
         <title>Healthy Bufo</title>
+        <meta name="description" content="Healthy Bufo helps you to eat healthier" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <HomeContainer />
     </>
