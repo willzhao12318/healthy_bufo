84b231d23a9fc79923c4a38b7ba55233ac7ac368
Will Zhao <wzhao2@flexport.com>
2024-12-02 13:42:29 +0000

    fix locale

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -8,7 +8,8 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en-US',
+    fallbackLng: "en",
+    load: "languageOnly",
     debug: process.env.NODE_ENV === 'development',
     interpolation: {
       escapeValue: false,
