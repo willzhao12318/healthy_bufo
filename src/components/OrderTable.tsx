ae68a286b2268872e2c27b20416f2d73deed99ee
Will Zhao <wzhao2@flexport.com>
2024-12-03 20:13:17 +0800

    remove modal

--- src/components/OrderTable.tsx
+++ src/components/OrderTable.tsx
@@ -1,26 +1,13 @@
 import { OrderTab } from "@/utils/type";
-import { Card, Col, Modal, Row, Space } from "antd";
+import { Card, Col, Row, Space } from "antd";
 import { compareTabType } from "@/utils/utils";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { useGetOrders } from "@/client/controller";
 import Image from "next/image";
 
 export default function OrderTable() {
   const { t, i18n } = useTranslation();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
 
   const { data: rawData } = useGetOrders();
 
@@ -42,52 +29,39 @@ export default function OrderTable() {
   }, [rawData]);
 
   return (
-    <>
-      <Space
-        direction="vertical"
-        size="middle"
-        style={{ width: "100%", height: "100%", alignItems: "flex-start", overflowY: "scroll" }}
-      >
-        {Object.entries(data).map(([time, tabs]) => (
-          <Card size="small" title={time} style={{ maxWidth: "900px" }} key={time}>
-            <Row gutter={16}>
-              {tabs.sort(compareTabType).map((tab) => (
-                <Col span={8} key={tab.id}>
-                  <Card
-                    hoverable
-                    size="small"
-                    cover={
-                      <Image
-                        src={`/static/dishes/${tab.orderedDish?.id}.png`}
-                        alt={tab.orderedDish?.id ?? ""}
-                        width={300}
-                        height={300}
-                      />
-                    }
-                    style={{ width: "300px" }}
-                    onClick={showModal}
-                  >
-                    <Card.Meta
-                      title={i18n.language === "zh" ? tab.orderedDish?.chineseName : tab.orderedDish?.englishName}
-                      description={t(tab.type)}
+    <Space
+      direction="vertical"
+      size="middle"
+      style={{ width: "100%", height: "100%", alignItems: "flex-start", overflowY: "scroll" }}
+    >
+      {Object.entries(data).map(([time, tabs]) => (
+        <Card size="small" title={time} style={{ maxWidth: "900px" }} key={time}>
+          <Row gutter={16}>
+            {tabs.sort(compareTabType).map((tab) => (
+              <Col span={8} key={tab.id}>
+                <Card
+                  hoverable
+                  size="small"
+                  cover={
+                    <Image
+                      src={`/static/dishes/${tab.orderedDish?.id}.png`}
+                      alt={tab.orderedDish?.id ?? ""}
+                      width={300}
+                      height={300}
                     />
-                  </Card>
-                </Col>
-              ))}
-            </Row>
-          </Card>
-        ))}
-      </Space>
-      <Modal
-        open={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-        centered
-        cancelText={t("cancel")}
-        okText={t("confirm")}
-      >
-        <p>Some contents...</p>
-      </Modal>
-    </>
+                  }
+                  style={{ width: "300px" }}
+                >
+                  <Card.Meta
+                    title={i18n.language === "zh" ? tab.orderedDish?.chineseName : tab.orderedDish?.englishName}
+                    description={t(tab.type)}
+                  />
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Card>
+      ))}
+    </Space>
   );
 }
